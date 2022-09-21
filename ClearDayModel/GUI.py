336d85df3bb74ee10ce721858c81e98ea85ac8ce
Asa Guest <acg70@duke.edu>
2022-09-21 17:53:24 -0400

    Plot added

--- ClearDayModel/GUI.py
+++ ClearDayModel/GUI.py
@@ -1,9 +1,10 @@
 import math
 import tkinter as tk
-from tkinter import DoubleVar, IntVar, ttk, Label
+from tkinter import DoubleVar, IntVar, ttk, Label, StringVar, Entry
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 import numpy as np
+from scipy.interpolate import InterpolatedUnivariateSpline
 
 
 class ClearDayGui:
@@ -11,26 +12,34 @@ class ClearDayGui:
         self.root = root
         self.root.geometry('1050x750')
         self.root.resizable(False, False)
-        self.add_slide_bars()
+        self.slide_bars_place()
+
+        self.solar_time = np.arange(-12, 12.1, .5)
+        self.Inso_b_n = 0
         self.plot_place()
-        self.solar_time = np.arange(-12, 12.1, .1)
+        self.caculation_static_labels()
 
         self.root.mainloop()
 
         pass
 
-    def add_slide_bars(self):
+    # Building GUI
+
+    def slide_bars_place(self):
         x_pos = 10
         label_spacing = 110
 
         self.day_var = IntVar()
         self.day_var.set(1)
+
         day = Label(self.root, text="Day: ").place(x=x_pos + label_spacing, y=10)
         day_label = Label(self.root, text=str(self.day_var.get()))
-        day_label.place(x=x_pos + label_spacing + 50, y=10)
+        # day_label.place(x=x_pos + label_spacing + 50, y=10)
         day_slide = ttk.Scale(self.root, from_=1, to=365, variable=self.day_var,
                               command=lambda x: self.slider_change(slider_var=self.day_var, slider_label=day_label))
         day_slide.place(x=x_pos, y=10)
+        day_e = Entry(self.root, textvariable=self.day_var)
+        day_e.place(x=x_pos + label_spacing + 50, y=10, )
 
         self.altitude_var = DoubleVar()
         self.altitude_var.set(0.1)
@@ -52,42 +61,14 @@ class ClearDayGui:
                                    command=lambda x: self.slider_change(slider_var=self.latitude_var,
                                                                         slider_label=lat_label))
         latitude_slide.place(x=x_pos, y=50)
-
-    def slider_change(self, slider_var, slider_label):
-        slider_label.configure(text="{:.2f}".format(slider_var.get()))
-        self.calculation_constants()
-        self.varying_params()
-        self.update_plot()
-
-    def varying_params(self):
-        self.omega = self.solar_time * .2618
-        self.sin_alpha = self.sin_delta * math.sin(self.phi) + math.cos(math.asin(self.sin_delta)) * np.cos(
-            self.omega) * math.cos(self.phi)
-        self.k_sin_alpha = -self.K / self.sin_alpha
-        self.raw_cd_factor = self.A0 + self.A1 * np.exp(self.k_sin_alpha)
-        self.cd_factor = []
-        for el in self.raw_cd_factor:
-            if el > 1:
-                self.cd_factor.append(0)
-            else:
-                self.cd_factor.append(el)
-        self.cd_factor = np.asarray(self.cd_factor)
-
-    def calculation_constants(self):
-        self.day_angle = (self.day_var.get() - 173) * 0.017453
-        self.declination_angle = math.asin(.39795 * math.cos(self.day_angle)) * 57.2957
-        self.phi = self.latitude_var.get() / 87.2957
-        self.sin_delta = .39795 * math.cos(self.day_angle)
-        self.A0 = .4237 - .00821 * (6 - self.altitude_var.get()) ** 2
-        self.A1 = .5055 - .00595 * (6 - self.altitude_var.get()) ** 2
-        self.K = .2711 + .1858 * (2.5 - self.altitude_var.get()) ** 2
+        self.root.bind("<Return>", lambda x: self.slider_change(slider_var=self.day_var, slider_label=day_label))
 
     def plot_place(self):
         self.fig = Figure(figsize=(8, 7), )
         self.plot1 = self.fig.add_subplot(111)
         self.fig.suptitle('Clear Day Insolation Factor')
-        self.fig.supxlabel('Solar Hour')
-        self.fig.supylabel('Insolation')
+        self.fig.supxlabel('Solar Hour (GMT)')
+        self.fig.supylabel('Insolation Factor')
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
         self.canvas.draw()
         # placing the self.canvas on the Tkinter window
@@ -101,10 +82,61 @@ class ClearDayGui:
         # placing the toolbar on the Tkinter window
         self.canvas.get_tk_widget().place(x=300, y=10)
 
+    def caculation_static_labels(self):
+        inso_label = Label(self.root, text="Bean Normal Insolation:")
+        inso_label.place(x=10, y=100)
+        self.inso_label = Label(self.root, text="{:.2f}".format(self.Inso_b_n * 1370) + "W/m^2")
+        self.inso_label.place(x=190, y=100)
+
+    def calculation_labels(self):
+        try:
+            self.inso_label.configure(text="{:.2f}".format(self.Inso_b_n * 1370))
+        except:
+            pass
+
+    def slider_change(self, slider_var, slider_label):
+        slider_label.configure(text="{:.2f}".format(slider_var.get()))
+        self.calculation_constants()
+        self.varying_params()
+        self.update_plot()
+        self.calculation_labels()
+
+    def varying_params(self):
+        try:
+            self.omega = self.solar_time * .2618
+            self.sin_alpha = self.sin_delta * math.sin(self.phi) + math.cos(math.asin(self.sin_delta)) * np.cos(
+                self.omega) * math.cos(self.phi)
+            self.k_sin_alpha = -self.K / self.sin_alpha
+            self.raw_cd_factor = self.A0 + self.A1 * np.exp(self.k_sin_alpha)
+            self.cd_factor = []
+            for el in self.raw_cd_factor:
+                if el > 1:
+                    self.cd_factor.append(0)
+                else:
+                    self.cd_factor.append(el)
+            self.cd_factor = np.asarray(self.cd_factor)
+        except:
+            pass
+
+    def calculation_constants(self):
+        self.day_angle = (self.day_var.get() - 173) * 0.017453
+        self.declination_angle = math.asin(.39795 * math.cos(self.day_angle)) * 57.2957
+        self.phi = self.latitude_var.get() / 87.2957
+        self.sin_delta = .39795 * math.cos(self.day_angle)
+        self.A0 = .4237 - .00821 * (6 - self.altitude_var.get()) ** 2
+        self.A1 = .5055 - .00595 * (6 - self.altitude_var.get()) ** 2
+        self.K = .2711 + .1858 * (2.5 - self.altitude_var.get()) ** 2
+
     def update_plot(self):
         try:
             self.plot1.clear()
+            f = InterpolatedUnivariateSpline(list(self.solar_time), list(self.cd_factor), k=1)
+            xs = np.linspace(-12, 12, 500)
+            self.plot1.plot(xs, f(xs))
+
             self.plot1.scatter(list(self.solar_time), list(self.cd_factor))
+            self.fig.legend(['Spline for integration', 'Discrete Points'], loc='upper left')
+            self.Inso_b_n = f.integral(0, 4)
             self.canvas.draw()
         except AttributeError:
             pass
