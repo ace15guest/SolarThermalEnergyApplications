e563e66c7546da11c9413e973baad0257bdab481
Asa Guest <acg70@duke.edu>
2022-10-03 21:45:29 -0400

    Project File Added

--- Projects/1D_Conduction_FDM/Dirichlet.py
+++ Projects/1D_Conduction_FDM/Dirichlet.py
@@ -1,84 +1,84 @@
-#Use these packages to build the diagonal matrix
-from scipy.sparse import diags
+#%%
 from scipy.integrate import quad
 import numpy as np
+import  matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 # Setting BC's
 T_0 = 100
 T_F = 10
-l=1
 # setting conductivity to a random (physically realisable) value
-k = 1
-# k = [np.ones(N-1),1*np.ones(N),np.ones(N-1)]
-offset = [-1,0,1]
-
-A = diags(k,offset).toarray()
+k = .3
+# Length of the rod
+length=10
+# Number of Nodes
+N = 4
 def n_j(x):
-    return 1-x/l
+    return x/length
 def n_i(x):
-    return x/l
+    return 1-x/length
 def dni_dx(x):
-    return (-x/l)
+    return (-1/length)
+def dnj_dx(x):
+    return 1/length
 def dni_dx2(x):
     return dni_dx(x)*dni_dx(x)
-def dnj_dx(x):
-    return x/l
+# Convenvtion term
+def P(x):
+    return 0*x
 def dnj_dx2(x):
     return dni_dx(x)*dni_dx(x)
-def P(x):
-    return 0
 def Q(x):
-    return 0
-
+    return 0*x
 def pn_i2(x):
     return P(x)*n_i(x)*n_i(x)
-
+def pn_j2(x):
+    return P(x)*n_j(x)*n_j(x)
 def dn_idn_j_dx(x):
     return dni_dx(x)*dnj_dx(x)
-
 def pn_in_j(x):
     return P(x)*n_i(x)*n_j(x)
-
-
-
 def dirichlet(n, bc1, bc2, l):
     """
 
-    :param n:
-    :param bc1:
-    :param bc2:
-    :param l:
-    :return:
+    :param n: Number of Nodes
+    :param bc1: Boundary condition at x = 0
+    :param bc2: Boundary Condition at x = L
+    :param l: Length of cross-section
+    :return: A matrix as np array, u as np array and x as np arraay
     """
     import numpy as np
     x = np.linspace(0.0, l, n)
 
     A = np.zeros([n,n])
-    b = np.zeros(n)
+    b = np.zeros(n-2)
     start = 0
-
-
-    step = l+n
+    step = l/n
     for i in range(0, n):
         end = start + step
-        int_range = [start, end]
-        if (i == 0):
+
+        if i == 0:
             # Diagonal
             A[i, i] = k*quad(dni_dx2, start, end)[0] - quad(pn_i2, start, end)[0]
             # Upper Diagonal
             A[i, i+1] = k*quad(dn_idn_j_dx, start, end)[0] - quad(pn_in_j, start, end)[0]
-            b[i] = bc1
-        elif (i < n-1):
+            # b[i] = 0
+        elif i < n-1:
             # Lower Diagonal
             A[i, i-1] = k*quad(dn_idn_j_dx, start, end)[0] - quad(pn_in_j, start, end)[0]
-            # Diagonal
-            A[i, i] = 1
+            # Diagonal (Int{xi}^xJ + Int{xj}^xk)
+            A[i, i] = (k*quad(dnj_dx2, start, end)[0] - quad(pn_j2, start, end)[0]) + (k*quad(dnj_dx2, end, end+step)[0] - quad(pn_j2, start, end+step)[0])
             # Upper Diagonal
-            A[i ,i +1] = k*quad(dn_idn_j_dx, start, end)[0] - quad(pn_in_j, start, end)[0]
-
+            A[i ,i +1] = k*quad(dn_idn_j_dx, end, end+step)[0] - quad(pn_in_j, end, end+step)[0]
         else:
-            A[i, i] = 1.0
-            b[i] =bc2
+            A[i, i] =  k*quad(dnj_dx2, end, end+step)[0] - quad(pn_j2, start, end)[0]
+            A[i, i-1] =  k*quad(dn_idn_j_dx, end, end+step)[0] - quad(pn_in_j, start, end)[0]
+            # b[i] = 0
         start+=step
+    A = [list(A[i][1:-1]) for i in range(np.shape(A)[0]-2)]
     u = np.linalg.solve(A, b)
-    return A, u
-dirichlet(10, 1, 2, 10)
+    return A, u, x
+_, u, x = dirichlet(1000, T_0, T_F, length)
+
+plt.plot(x[2:], u)
+plt.show()
